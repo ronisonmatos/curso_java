41da3787598f62964f931d5258fa7e2934bace50
Ronison Matos <ronisonmaria@gmail.com>
2020-11-02 17:00:05 -0300

    Importe Project
    First Commit

---
+++ course_java/src/course_java/exercicio/Exercicio_01.java
@@ -0,0 +1,25 @@
+package course_java.exercicio;
+
+public class Exercicio_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		String product1 = "Computer";
+		String product2 = "Office desk";
+		
+		int age = 30;
+		int code = 5290;
+		char genter = 'F';
+		
+		double price1 = 2100.0;
+		double price2 = 650.50;
+		double measure = 53.234567;
+		
+		System.out.printf("Products: %n %s which price is $ %.1f", product1,price1);
+		
+		
+	}
+
+}

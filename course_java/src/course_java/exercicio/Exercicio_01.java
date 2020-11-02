d6507d384643fb18e14f0cf08c863d4de5904b71
Ronison <Ronison@DESKTOP-DT52J7O>
2020-11-02 17:40:06 -0300

    Conclusão do Exercicio 01

--- course_java/src/course_java/exercicio/Exercicio_01.java
+++ course_java/src/course_java/exercicio/Exercicio_01.java
@@ -1,5 +1,8 @@
 package course_java.exercicio;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class Exercicio_01 {
 
 	public static void main(String[] args) {
@@ -17,7 +20,15 @@ public class Exercicio_01 {
 		double price2 = 650.50;
 		double measure = 53.234567;
 		
-		System.out.printf("Products: %n %s which price is $ %.1f", product1,price1);
+		System.out.printf("Products: %n %s which price is $ %.1f %n %s, whitch price is $ %.2f%n", product1,price1,product2,price2);
+		System.out.printf("%nRecord: %d years old, code %d and gender: %s%n", age,code,genter);
+		System.out.printf("%nMeasue with eitght decimal palces: %f%n", measure);
+		System.out.printf("Rouded (three decimal places): %.3f", measure);
+		
+		Locale l = new Locale("pt","BR");
+		NumberFormat virgula = NumberFormat.getInstance(l);
+		
+		System.out.printf("US decimal point: %s",virgula.format(measure));
 		
 		
 	}
